917a48b38afc25a4c2ed365869dae84c116d75f7
Va-rx <frytkaspl@gmail.com>
2024-03-20 13:29:22 +0100

    function rename due to overshadow

--- backend/statistics/univariate/numerical.py
+++ backend/statistics/univariate/numerical.py
@@ -4,51 +4,51 @@ import matplotlib.pyplot as plt
 from scipy.stats import skew, kurtosis
 
 
-def min(column):
+def get_min(column):
     return min(column)
 
 
-def max(column):
+def get_max(column):
     return max(column)
 
 
-def mean(column):
+def get_mean(column):
     return st.mean(column)
 
 
-def median(column):
+def get_median(column):
     return st.median(column)
 
 
-def mode(column):
+def get_mode(column):
     return st.mode(column)
 
 
-def range(column):
+def get_range(column):
     return max(column) - min(column)
 
 
-def quantiles(column):
+def get_quantiles(column):
     pass
 
 
-def variance(column):
+def get_variance(column):
     return st.variance(column)
 
 
-def std_dev(column):
+def get_std_dev(column):
     return st.stdev(column)
 
 
-def coef_var(column):
-    return (std_dev(column) / mean(column)) * 100
+def get_coef_var(column):
+    return (get_std_dev(column) / get_mean(column)) * 100
 
 
-def skew(column):
+def get_skew(column):
     return skew(column)
 
 
-def kurtosis(column):
+def get_kurtosis(column):
     return kurtosis(column)
 
 
@@ -62,5 +62,5 @@ def histogram_graph(column):
     plt.show()
 
 
-arr = [1, 1, 1, 20, 30, 31, 31, 22, 22, 12, 30, 49]
-histogram_graph(arr)
+# arr = [1, 1, 1, 20, 30, 31, 31, 22, 22, 12, 30, 49]
+# histogram_graph(arr)
