a551fb0ea433c3911dfc619b7c77138acd97bcb2
Marcin Bereta <412994@student.agh.edu.pl>
2024-04-10 18:39:41 +0200

    fixes

--- backend/routes/stats/data_builder.py
+++ backend/routes/stats/data_builder.py
@@ -8,13 +8,13 @@ def get_data(col_csv, col_type, col_name, userId, fileId):
     arr_data = csv_to_arr(col_csv)
     if col_type == 'number':
         print("")
-        # data = get_num_data(col_csv)
-        # graphs.append(numerical.histogram_graph(arr_data, col_name, userId, fileId))
-        # graphs.append(numerical.box_graph(arr_data, col_name, userId, fileId))
+        data = get_num_data(col_csv)
+        graphs.append(numerical.histogram_graph(arr_data, col_name, userId, fileId))
+        graphs.append(numerical.box_graph(arr_data, col_name, userId, fileId))
     elif col_type == 'text':
         print("")
-        # graphs.append(categorical.count_graph(arr_data, col_name, userId, fileId))
-        # graphs.append(categorical.count_perc_graph(arr_data, col_name, userId, fileId))
+        graphs.append(categorical.count_graph(arr_data, col_name, userId, fileId))
+        graphs.append(categorical.count_perc_graph(arr_data, col_name, userId, fileId))
     return data, graphs
 
 
